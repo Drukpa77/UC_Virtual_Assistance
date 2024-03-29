27dd298dfa76850402634e3b67f278265e36d494
Polowis <39208974+polowis@users.noreply.github.com>
2024-03-29 13:42:12 +1100

    add optimizer function

--- src/config/trainer.py
+++ src/config/trainer.py
@@ -5,6 +5,7 @@ class TrainerConfig:
         self.accumulation_step = 8
         self.train_batch_size = 4
         self.valid_batch_size = 32
+        self.learning_rate = 3e-5
 
     def load_config(self) -> None:
         pass

042423a3fd8f539216c290424f2853b40027bda1
Polowis <39208974+polowis@users.noreply.github.com>
2024-04-10 18:18:55 +1000

    add user icon

--- web/src/layouts/ChatApp/index.tsx
+++ web/src/layouts/ChatApp/index.tsx
@@ -5,7 +5,10 @@ import ArrowUp from '@/components/icons/ArrowUp';
 import Row from '@/components/ui/Row';
 import ThumbsUpIcon from '@/components/icons/ThumbsUpIcon';
 import ThumbsDownIcon from '@/components/icons/ThumbsDownIcon';
+import Box from '@/components/ui/Box';
 import styles from './style.module.css';
+import AccountIcon from '@/components/icons/AccountIcon';
+import RobotIcon from '@/components/icons/RobotIcon';
 
 export default function ChatApp() {
   return (
@@ -14,19 +17,34 @@ export default function ChatApp() {
         <ChatAppBar/>
       </div>
       <div className={styles['chat-area']}>
-        <ChatMessage message='Hello world'/>
-        <ChatMessage message='Hi'/>
-        <Row pl={3}>
-          <ThumbsUpIcon className={styles['feedback-icon']}/>
-          <ThumbsDownIcon className={`${styles['feedback-icon']} ${styles['feedback-icon-right']}`}/>
-        </Row>
+          <Row>
+            <AccountIcon className={styles['user-icon']}/>
+            <Box>
+              <Box pl={3}><b>User</b></Box>
+              <ChatMessage message='Hello world'/>
+            </Box>
+          </Row>
+          <Row>
+            <RobotIcon className={styles['user-icon']}/>
+            <Box>
+              <Box pl={3}><b>Bot</b></Box>
+              <ChatMessage message='Hi'/>
+              <Row pl={3}>
+                <ThumbsUpIcon className={styles['feedback-icon']}/>
+                <ThumbsDownIcon className={`${styles['feedback-icon']} ${styles['feedback-icon-right']}`}/>
+              </Row>
+            </Box>
+          </Row>
       </div>
       <div className={styles['chat-input']}>
         <form className={styles['chat-form']}>
-          <Input className={styles.input} placeholder='type your message'/>
-          <button type="submit" className={styles['submit-button']}>
-            <ArrowUp/>
-          </button>
+          <Box className={styles['input-container']}>
+            <Input className={styles.input} placeholder='type your message'/>
+            <button type="submit" className={styles['submit-button']}>
+              <ArrowUp/>
+            </button>
+          </Box>
+          
           
         </form>
         <div>UC Bot can make mistakes, consider checking information</div>
